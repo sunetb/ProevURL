b1ca53a7eabbaabfbd3480db330fce900fcf3df4
sune <sunetb@gmail.com>
2022-03-25 11:11:24 +0100

    Self fik objekt-variabelen til at virke

--- main.py
+++ main.py
@@ -23,19 +23,19 @@ class MyHTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
         # print("Start tag:", tag)
         mdo.current_tag = tag
-        my_global_var = tag # Den er gal her
+        my_global_var = tag                         # Den er gal her
         self.my_object_global_var = tag
 
 
     def handle_data(self, data):
-        if mdo.current_tag == 'tr': #virker
+        if mdo.current_tag == 'tr':                 #virker
             print("Datobject has 'tr'")
-        if my_global_var == 'tr':   #Virker ikke
+        if my_global_var == 'tr':                   #Virker ikke
             print("global var has 'tr'")
-        if self.my_object_global_var == 'tr':    #Crasher uden 'self'
+        if self.my_object_global_var == 'tr':       #Crasher uden 'self'
             print("Object global var has 'tr'")
 
-print(html_text)
+#print(html_text)
 parser = MyHTMLParser()
 parser.feed(html_text)
 
