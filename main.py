1b1b7a8700703a926926885d187fdb73ab583a3b
sune <sunetb@gmail.com>
2022-03-25 10:52:32 +0100

    Forenklet og tilføjet variabel til parserobjektet

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ link = "https://certificering.tbst.dk/?page=1&Fagdisciplin=0&Certificeringsnivea
 f = requests.get(link)
 
 html_text = f.text
+
 my_global_var = ""
 
 
@@ -16,32 +17,26 @@ class MyDataObject:
 mdo = MyDataObject()
 
 
-
-the_data = []
-
-
 class MyHTMLParser(HTMLParser):
+    my_object_global_var = ""
 
     def handle_starttag(self, tag, attrs):
         # print("Start tag:", tag)
         mdo.current_tag = tag
         my_global_var = tag # Den er gal her
+        my_object_global_var = tag
 
-
-
-    def handle_endtag(self, tag):
-        pass
-        #print("End tag  :", tag) ##TODO: /tr
 
     def handle_data(self, data):
-        if mdo.current_tag == 'tr':
+        if mdo.current_tag == 'tr': #virker
             print("Datobject has 'tr'")
-        if my_global_var == 'tr':
+        if my_global_var == 'tr':   #Virker ikke
             print("global var has 'tr'")
+       # if my_object_global_var == 'tr':    #Crasher?
+       #     print("Object global var has 'tr'")
 
 print(html_text)
 parser = MyHTMLParser()
 parser.feed(html_text)
-print("===============tjek data================")
-print(len(the_data))
+
 
